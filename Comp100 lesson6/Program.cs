ef0e354c8972ea173f3003ec61bb43f98bc34f9d
pzsbbfan <95864657+pzsbbfan@users.noreply.github.com>
2022-01-05 16:17:48 -0500

    Generate random number

--- Comp100 lesson6/Program.cs
+++ Comp100 lesson6/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int[] dice;
+dice = new int[6];
+
+Random rand = new Random();
+for (int i = 0; i < 6; i++)
+{
+    dice[i] = rand.Next();
+    Console.WriteLine(rand.Next(i));
+}
+
+Console.ReadKey();
