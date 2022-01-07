88fe323ca6d293b2aa7cf074bf731f42f8d06a93
pzsbbfan <95864657+pzsbbfan@users.noreply.github.com>
2022-01-07 15:14:21 -0500

    Deck of card + Draw a random card

--- Comp100 lesson6/Program.cs
+++ Comp100 lesson6/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-int[] faces = new int[13];
+string[] faces = new string[13];
 string[] suits = {"Club","Diamonds","Hearts","Spades"};
 string[] cards = new string[52];
 int counter = 0;
@@ -8,18 +8,58 @@ int counter = 0;
 
 for (int face  = 0; face < faces.Length; face ++) 
 {
-    faces[face] = face + 1;
+    faces[face] = (face + 1).ToString();
 }
 
 for (int suit = 0; suit < suits.Length; suit++) 
 {
     for (int index = 0; index < faces.Length; index++)
     {
-        cards[counter] = faces[index].ToString() + "of" + suits[suit]; 
+         switch (counter) 
+         {
+             case 0: 
+                 {
+                     faces[index] = "Ace ";
+                     break;
+                 }
+             case 10:
+                 {
+                     faces[index] = "Jack ";
+                     break;
+                 }
+
+
+             case 11:
+                 {
+                     faces[index] = "Queen";
+                     break;
+                 }
+
+             case 12:
+                 {
+                     faces[index] = "King ";
+                     break;
+                 }
+
+    }
+
+
+
+    cards[counter] = faces[index].ToString() + "of " + suits[suit]; 
         Console.WriteLine(cards[counter]);
         counter++;
-    }
-} 
+        }
+}
+Console.WriteLine();
+Random rand = new Random();
+
+for (int i = 0; i < 6; i++)
+{
+    int randomIndex = rand.Next(cards.Length);
+    Console.WriteLine(cards[randomIndex]);
+}
+
+
 
 
 Console.ReadKey(); 
