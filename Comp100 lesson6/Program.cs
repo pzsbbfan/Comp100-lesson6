cc256b495b76735d5562a10b1827745d7f82dafa
pzsbbfan <95864657+pzsbbfan@users.noreply.github.com>
2022-01-12 10:47:00 -0500

    reorginized and add comment

--- Comp100 lesson6/Program.cs
+++ Comp100 lesson6/Program.cs
@@ -1,86 +1,69 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-string[] faces = new string[13];
-string[] suits = {"Club","Diamonds","Hearts","Spades"};
-string[] cards = new string[52];
-string search = "";
+// Declare the arrays
+string[] faces = new string [13];
+string[] suits = {"Club","Diamond","Heart","Spades"};
+
+// initialized the deck of card array
+string[] deckOfCards = new string[52];
+
+//initialized counter to track each element in deckOfCards[]
 int counter = 0;
-int location =0;
-bool cardfound = false;
-for (int face  = 0; face < faces.Length; face ++) 
+
+//initialized a string for finding a card
+string findCard = "";
+
+//initialized the position of card want to find 
+int position = 0;
+
+//initialized a boolean for find card loop to go
+bool foundCard = false;
+
+// fixing the index and values of faces[] 
+for (int face = 0; face < faces.Length; face++)
 {
     faces[face] = (face + 1).ToString();
 }
 
-for (int suit = 0; suit < suits.Length; suit++) 
+// combine faces[] with suits[] using nested loops 
+for (int suit = 0; suit < suits.Length; suit++)
 {
-    for (int index = 0; index < faces.Length; index++)
+    for (int card = 0; card < faces.Length; card++)
     {
-         switch (counter) 
-         {
-             case 0: 
-                 {
-                     faces[index] = "Ace";
-                     break;
-                 }
-             case 10:
-                 {
-                     faces[index] = "Jack";
-                     break;
-                 }
-
-
-             case 11:
-                 {
-                     faces[index] = "Queen";
-                     break;
-                 }
-
-             case 12:
-                 {
-                     faces[index] = "King";
-                     break;
-                 }
-
-    }
-
-
-
-    cards[counter] = faces[index].ToString() + " of " + suits[suit]; 
+        // pass the vaulue to deckOfCards[] 
+        deckOfCards[counter] = faces[card].ToString() + " of " + suits[suit];
+        Console.WriteLine(deckOfCards[counter]);
         counter++;
-        }
-}
-foreach (string card in cards)
-{
-    Console.WriteLine(card);
-}
-Console.WriteLine();
-Random rand = new Random();
-int randomCardIndex = rand.Next(52);
-Console.WriteLine("Randomly draw a card:");
-Console.WriteLine(cards[randomCardIndex]);
-Console.WriteLine();
-
-
-Console.WriteLine("Please type the card you want to find in the deck");
-search =  Console.ReadLine();
-Console.WriteLine("you entered {0}",search);
-
-
-foreach (string Card in cards) 
-{
-    if (Card == search)
-    {
-        Console.WriteLine("this card is located at {0}", location + 1);
-        cardfound = true;
     }
-    location++;
 }
-if (cardfound == false) 
+Console.WriteLine();
+// Generates Random in order to draw a random card 
+Random random = new Random();
+
+// Generates random index for drawing a card
+int randomIndex = random.Next(52);
+Console.WriteLine("The random card you draw from the deck is {0}",deckOfCards[randomIndex]);
+Console.WriteLine();
+
+// Finding the place of a card that user inputs
+// prompt a card that user want to find
+Console.WriteLine("Enter the card you want to find");
+findCard = Console.ReadLine();
+// using foreach loop to go through everysinge element in deckOfCard[]
+foreach(string card in deckOfCards) 
+{
+    // using if statement to check
+    if (findCard == deckOfCards[position])
+    { 
+        Console.WriteLine("{0} is located at {1}th position of the deck",card,position + 1);
+        foundCard = true;
+    }
+    position++;
+}
+
+if (foundCard == false) 
 {
     Console.WriteLine("card not found");
 }
-
-
 
 Console.ReadKey(); 
