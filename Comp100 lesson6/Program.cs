0321ca5f62b221a2d62b033292bbe1c4a2c45a4d
pzsbbfan <95864657+pzsbbfan@users.noreply.github.com>
2022-01-12 00:23:05 -0500

    Located a card in a deck

--- Comp100 lesson6/Program.cs
+++ Comp100 lesson6/Program.cs
@@ -3,8 +3,10 @@
 string[] faces = new string[13];
 string[] suits = {"Club","Diamonds","Hearts","Spades"};
 string[] cards = new string[52];
+string search = "";
 int counter = 0;
-
+int location =0;
+bool cardfound = false;
 for (int face  = 0; face < faces.Length; face ++) 
 {
     faces[face] = (face + 1).ToString();
@@ -18,12 +20,12 @@ for (int suit = 0; suit < suits.Length; suit++)
          {
              case 0: 
                  {
-                     faces[index] = "Ace ";
+                     faces[index] = "Ace";
                      break;
                  }
              case 10:
                  {
-                     faces[index] = "Jack ";
+                     faces[index] = "Jack";
                      break;
                  }
 
@@ -36,7 +38,7 @@ for (int suit = 0; suit < suits.Length; suit++)
 
              case 12:
                  {
-                     faces[index] = "King ";
+                     faces[index] = "King";
                      break;
                  }
 
@@ -44,12 +46,11 @@ for (int suit = 0; suit < suits.Length; suit++)
 
 
 
-    cards[counter] = faces[index].ToString() + "of " + suits[suit]; 
+    cards[counter] = faces[index].ToString() + " of " + suits[suit]; 
         counter++;
         }
 }
-
-foreach (string card in cards) 
+foreach (string card in cards)
 {
     Console.WriteLine(card);
 }
@@ -58,4 +59,28 @@ Random rand = new Random();
 int randomCardIndex = rand.Next(52);
 Console.WriteLine("Randomly draw a card:");
 Console.WriteLine(cards[randomCardIndex]);
+Console.WriteLine();
+
+
+Console.WriteLine("Please type the card you want to find in the deck");
+search =  Console.ReadLine();
+Console.WriteLine("you entered {0}",search);
+
+
+foreach (string Card in cards) 
+{
+    if (Card == search)
+    {
+        Console.WriteLine("this card is located at {0}", location + 1);
+        cardfound = true;
+    }
+    location++;
+}
+if (cardfound == false) 
+{
+    Console.WriteLine("card not found");
+}
+
+
+
 Console.ReadKey(); 
